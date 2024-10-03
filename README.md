1cfe05961983cdaf54a7d1bd919fb75911e50f71
JonyNhoque <jgabriel.nhoque@gmail.com>
2024-10-03 00:02:09 -0300

    feature: - Remove .env desnecessário; - Adiciona o front no CORS; - Lista os usuários cadastrados no front - WIP;

---
+++ README.md
@@ -0,0 +1,9 @@
+Para iniciar o projeto: 
+
+Dentro da pasta Frontend e da pasta Backend:
+`` yarn install  ``
+
+Dentro de Backend: 
+`` docker-compose up -d ``
+
+Acesse: http://localhost:8082/
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ContatosModule } from './contatos/contatos.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ContatosModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI)
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
