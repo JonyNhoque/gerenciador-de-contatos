10e673ae14fe5bb31780a5ccb3b20f68c974d3e8
JonyNhoque <jgabriel.nhoque@gmail.com>
2024-10-04 14:53:51 -0300

    feat: - Adiciona e configura prime-icons; - Ajusta tela de exibição de contatos;

--- frontend/src/contatos/ContatosView.vue
+++ frontend/src/contatos/ContatosView.vue
@@ -1,13 +1,88 @@
 <template>
   <div class="container mt-5">
-    <h2>Contatos</h2>
-    <p>Esta é a página de contatos.</p>
+    <div class="card">
+
+      <div class="card-header">
+        <h2>Contatos</h2>
+        <p>Gerencie seus contatos</p>
+      </div>
+
+      <div class="card-body">
+        <DataTable 
+          :value="contatos"
+        >
+          <Column field="nome" header="Nome"></Column>
+          <Column field="email" header="Email"></Column>
+          <Column field="endereco" header="Endereço"></Column>
+          <Column field="telefone" header="Telefone"></Column>
+          <Column header="Ações">
+            <template #body="slotProps">
+              <Button 
+                icon="pi pi-pencil" 
+                class="me-2"
+                @click="editContato(slotProps.data)"
+                aria-label="Editar"
+              />
+              <Button 
+                icon="pi pi-trash" 
+                @click="deleteContato(slotProps.data._id)"
+                aria-label="Excluir"
+              />
+            </template>
+          </Column>
+        </DataTable>
+
+        <div class="d-flex justify-content-start mt-3">
+          <Button label="Cadastrar contato" class="p-button p-button-primary" v-on:click="cadastrarUsuario()" />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { ref } from 'vue';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+
 export default {
-  name: 'ContatosView'
+  name: 'ContatosView',
+  components: {
+    DataTable,
+    Column,
+    Button
+  },
+  mounted(){
+    this.getContatos();
+  },
+  setup() {
+    const contatos = ref([]);
+    const contatoSelecionado = ref();
+
+    async function getContatos(){
+      try {
+        const response = await axios.get('http://localhost:3000/contatos')
+        return contatos.value = response.data;
+      } catch(error) {
+        console.error(error)
+      }
+    }
+
+    async function cadastrarUsuario(){
+      return
+    }
+    
+    return {
+      contatos,
+      contatoSelecionado,
+      getContatos,
+      cadastrarUsuario
+    }
+  }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import './style.scss';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
